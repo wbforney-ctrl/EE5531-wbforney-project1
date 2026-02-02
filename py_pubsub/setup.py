09e2d0dc96164944737a42902c37cd2f4a01a534
wbforney <wbforney@mtu.edu>
2026-02-02 02:30:49 -0500

    Task 5 commit 4

--- py_pubsub/setup.py
+++ py_pubsub/setup.py
@@ -25,6 +25,7 @@ setup(
     entry_points={
         'console_scripts': [
 		'talker = py_pubsub.publisher_member_function:main',
+		'listener = py_pubsub.subscriber_member_function:main',
         ],
     },
 )
